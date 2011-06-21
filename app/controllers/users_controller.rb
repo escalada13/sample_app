c1d5d71d5e6533ec8ae70a5a8934ea7ea9861b9d
oscar <oscar@webanddesignstudio.com>
2011-06-20 17:30:49 -0700

    Finished layout and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def new
-  @title = "Sign up"
+  @title = "Sign Up"
   end
 
 end
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -24,7 +24,21 @@ describe "LayoutLinks" do
 
   it "should have a signup page at '/signup'" do
     get '/signup'
-    response.should have_selector('title', :content => "Sign up")
+    response.should have_selector('title', :content => "Sign Up")
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Help"
+    response.should have_selector('title', :content => "Help") # fill in
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")# fill in
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")# fill in
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Sign Up")# fill in
   end
 end
 
